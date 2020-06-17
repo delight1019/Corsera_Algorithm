1f07489a87a0a8c9b1dd552b9f76478f340ea78e
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-06-17 21:55:56 +0900

    Stack

---
+++ Stack/src/ArrayStack.java
@@ -0,0 +1,41 @@
+public class ArrayStack<Item> {
+    private Item[] items;
+    private int N = 0;
+
+    public ArrayStack() {
+        items = (Item[]) new Object[1];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void push(Item item) {
+        if (N == items.length) {
+            resize(2 * items.length);
+        }
+
+        items[N++] = item;
+    }
+
+    public Item pop() {
+        Item item = items[--N];
+        items[N] = null;
+
+        if (N > 0 && N == items.length/4) {
+            resize(items.length / 2);
+        }
+
+        return item;
+    }
+
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
+
+        for (int i = 0; i < N; i++) {
+            copy[i] = items[i];
+        }
+
+        items = copy;
+    }
+}
